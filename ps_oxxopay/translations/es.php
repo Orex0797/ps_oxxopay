ed7feb21ed4fae507516ded4aeb1eba55a724be3
Orex0797 <naturistaenlinea97@gmail.com>
2025-02-26 13:27:39 -0600

    Delete ps_oxxopay directory

--- ps_oxxopay/translations/es.php
+++
@@ -1,7 +0,0 @@
-<?php
-global $_MODULE;
-$_MODULE = [];
-$_MODULE['<{ps_oxxopay}prestashop>ps_oxxopay_1a5f7c83315a5ea9c22d7fa4b02050ae'] = 'Pago en OXXO';
-$_MODULE['<{ps_oxxopay}prestashop>ps_oxxopay_4b5f3b5d4e67b4f8b2ff0eeb92c3b734'] = 'Permite a los clientes pagar en OXXO mediante depósito bancario.';
-$_MODULE['<{ps_oxxopay}prestashop>validation_0dc68d09f4b80c140c7fd2dfdb56d7bb'] = 'Tu pedido ha sido registrado. Realiza el pago en cualquier tienda OXXO.';
-$_MODULE['<{ps_oxxopay}prestashop>validation_3c00e7ef22f3e3dbb89c60e62d5f5402'] = 'Una vez confirmado el pago, tu pedido será procesado.';
